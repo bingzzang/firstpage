f5746ba1f7bf1d759cf4bdf46e5e78480067d266
Bing <eulvgi@gmail.com>
2022-01-14 01:04:25 +0900

    멤버인터페이스

--- firstpage/src/main/java/com/bing/first/service/MemberService.java
+++ firstpage/src/main/java/com/bing/first/service/MemberService.java
@@ -2,7 +2,15 @@ package com.bing.first.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
-public class MemberService {
+import com.bing.first.model.MemberVo;
 
+@Service
+public interface MemberService {
+
+	public int checkId(String userId);			//중복아이디 체크
+	
+	public void registerMember(MemberVo vo);	//회원가입
+	
+	public int remove(int idx);					//탈퇴(멤버삭제)	
+	
 }
